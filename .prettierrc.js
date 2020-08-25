822838da4815dbc78c76dc2955a38f1fa2f4eb49
pyjun01 <pyjun02@gmail.com>
2020-08-25 23:38:16 +0900

    initial commit!

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  singleQuote: true,
+  jsxSingleQuote: true,
+  semi: true,
+  useTabs: false,
+  tabWidth: 2,
+  trailingComma: 'all',
+  printWidth: 120,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'always',
+};
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # nonogram
+
 nonogram with javascript!
+<br />
+[nonogram wiki](https://ko.wikipedia.org/wiki/%EB%85%B8%EB%85%B8%EA%B7%B8%EB%9E%A8)
