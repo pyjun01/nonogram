2941cc8b1cf58c41c6f97965504d68ad04a62430
pyjun01 <pyjun02@gmail.com>
2020-09-23 00:33:22 +0900

    기본 Canvas 객체 추가, animationframe function 추가

--- src/nonogram.ts
+++ src/nonogram.ts
@@ -1,29 +1,60 @@
-const contextSetting = (ctx: CanvasRenderingContext2D) => {
-  ctx.lineCap = 'round';
-  ctx.lineJoin = 'round';
+interface CanvasInterface {
+  element?: HTMLCanvasElement;
+  context?: CanvasRenderingContext2D;
+  size: number;
+  setElement: (canvas: HTMLCanvasElement) => void;
+  setContext: (ctx: CanvasRenderingContext2D) => void;
+  setSize: () => void;
+  setCanvasSize: () => void;
+  contextSetting: () => void;
+  clear: () => void;
+}
+
+const Canvas: CanvasInterface = {
+  element: undefined,
+  context: undefined,
+  size: 0,
+  setElement(canvas: HTMLCanvasElement) {
+    this.element = canvas;
+  },
+  setSize() {
+    const min = Math.min.apply(null, [window.innerWidth, window.innerHeight]);
+    this.size = min <= 700 ? min : min - 200;
+
+    this.setCanvasSize();
+  },
+  setCanvasSize() {
+    if (this.element) {
+      this.element.width = this.size;
+      this.element.height = this.size;
+    }
+  },
+  setContext(ctx: CanvasRenderingContext2D) {
+    this.context = ctx;
+  },
+  contextSetting() {
+    if (this.context) {
+      this.context.lineCap = 'round';
+      this.context.lineJoin = 'round';
+      this.context.font = '16px sans-serif';
+      this.context.textAlign = 'center';
+      this.context.textBaseline = 'middle';
+    }
+  },
+  clear() {
+    this.context?.clearRect(0, 0, this.size, this.size);
+  },
 };
+const textHeight = 11.583999633789062;
 
-const getSize = () => {
-  const min = Math.min.apply(null, [window.innerWidth, window.innerHeight]);
+let mouseState = 0; // 0 = default, mouseup // 1 = mousedown // 2 = mousemove
 
-  if (min <= 700) {
-    return min;
-  }
-
-  return min - 200;
-};
-
-const eventSetting = (canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D) => {
-  let mouseState = 0;
-
+const eventSetting = () => {
   window.addEventListener('resize', (e) => {
-    const size = getSize();
-
-    canvas.width = size;
-    canvas.height = size;
+    Canvas.setSize();
   });
 
-  canvas.addEventListener('mousedown', (e) => {
+  Canvas.element?.addEventListener('mousedown', (e) => {
     console.log('mousedown', e);
     mouseState = 1;
   });
@@ -43,18 +74,71 @@ const eventSetting = (canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D) 
   });
 };
 
+const draw = () => {
+  Canvas.clear();
+  Canvas.context?.beginPath();
+  for (let i = 2; i < 8; i++) {
+    Canvas.context?.moveTo((Canvas.size / 8) * i, 0);
+    Canvas.context?.lineTo((Canvas.size / 8) * i, Canvas.size);
+    Canvas.context?.moveTo(0, (Canvas.size / 8) * i);
+    Canvas.context?.lineTo(Canvas.size, (Canvas.size / 8) * i);
+  }
+  Canvas.context?.stroke();
+  Canvas.context?.closePath();
+
+  Canvas.context?.fillText(`2`, (Canvas.size / 8) * 6.5, (Canvas.size / 8) * 0.5);
+  Canvas.context?.fillText(`1`, (Canvas.size / 8) * 2.5, (Canvas.size / 8) * 1.5);
+  Canvas.context?.fillText(`5`, (Canvas.size / 8) * 3.5, (Canvas.size / 8) * 1.5);
+  Canvas.context?.fillText(`2`, (Canvas.size / 8) * 4.5, (Canvas.size / 8) * 1.5);
+  Canvas.context?.fillText(`5`, (Canvas.size / 8) * 5.5, (Canvas.size / 8) * 1.5);
+  Canvas.context?.fillText(`1`, (Canvas.size / 8) * 6.5, (Canvas.size / 8) * 1.5);
+  Canvas.context?.fillText(`2`, (Canvas.size / 8) * 7.5, (Canvas.size / 8) * 1.5);
+
+  Canvas.context?.fillText(`2`, (Canvas.size / 8) * 0.5, (Canvas.size / 8) * 2.5);
+  Canvas.context?.fillText(`1`, (Canvas.size / 8) * 0.5, (Canvas.size / 8) * 3.5);
+  Canvas.context?.fillText(`1`, (Canvas.size / 8) * 0.5, (Canvas.size / 8) * 4.5);
+
+  Canvas.context?.fillText(`1`, (Canvas.size / 8) * 1.5, (Canvas.size / 8) * 2.5);
+  Canvas.context?.fillText(`3`, (Canvas.size / 8) * 1.5, (Canvas.size / 8) * 3.5);
+  Canvas.context?.fillText(`2`, (Canvas.size / 8) * 1.5, (Canvas.size / 8) * 4.5);
+  Canvas.context?.fillText(`3`, (Canvas.size / 8) * 1.5, (Canvas.size / 8) * 5.5);
+  Canvas.context?.fillText(`4`, (Canvas.size / 8) * 1.5, (Canvas.size / 8) * 6.5);
+  Canvas.context?.fillText(`1`, (Canvas.size / 8) * 1.5, (Canvas.size / 8) * 7.5);
+};
+
+const requestAnimation = () => {
+  let first = 0;
+  let now = undefined;
+  let count = 0;
+
+  const callback = () => {
+    draw();
+
+    now = performance.now();
+
+    if (now - first > 1000) {
+      console.log(count);
+      first = now;
+      count = 0;
+    } else {
+      count++;
+    }
+
+    window.requestAnimationFrame(callback);
+  };
+
+  window.requestAnimationFrame(callback);
+};
+
 export const init = (canvas: HTMLCanvasElement) => {
-  const size = getSize();
-
-  canvas.width = size;
-  canvas.height = size;
-
   const ctx = canvas.getContext('2d');
 
   if (!ctx) return;
 
-  contextSetting(ctx);
-  eventSetting(canvas, ctx);
-  console.log(canvas);
-  console.log(ctx);
+  Canvas.setElement(canvas);
+  Canvas.setContext(ctx);
+  Canvas.setSize();
+  Canvas.contextSetting();
+  eventSetting();
+  requestAnimation();
 };
